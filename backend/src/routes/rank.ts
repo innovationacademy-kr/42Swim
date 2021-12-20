0c000c897c15c474a00a171af86e080c9d2b01fe
hainho <hainho9704@gmail.com>
2021-12-20 16:13:41 +0900

    :lock: hidden-api : 월간랭킹 초기화 api 주소 env 파일로 이동

--- backend/src/routes/rank.ts
+++ backend/src/routes/rank.ts
@@ -1,6 +1,8 @@
+import dotenv from "dotenv";
+dotenv.config();
 import express from "express";
 import { RankController } from "../controllers/rank";
 
 export const rankRouter = express.Router();
 
-rankRouter.patch('/month/resetting', RankController.resetMonthRanking);
+rankRouter.patch(process.env.MONTHRANK_RESET, RankController.resetMonthRanking);
